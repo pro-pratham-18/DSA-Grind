65bea76b24643f9fb01c630237067315164e4019
pro-pratham-18 <itz.prathathamesh18@gmail.com>
2026-04-21 21:32:39 +0530

    solutions added

--- LeetCode Sol/Sort Colors/sortColors.cpp
+++ LeetCode Sol/Sort Colors/sortColors.cpp
@@ -57,7 +57,63 @@
 
 // when elemnt is 0 , swap with start pointer , and increase both start and middle by 1 (s++,m++)
 // when elemnt is 1 , only increase middle by 1 (m++)
-// when element is 2, swap with the end pointer anf decrease end pointer by 1 (e--)
+// when element is 2, swap with the end pointer an ddecrease end pointer by 1 (e--)
+
+//--------------------------------------------------------------------------------------------------------------------
+
+// 4 color sorting way using two partition , TC - O(n) (approach)
+
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// vector <int> color_sort(vector <int> nums){
+//     int n=nums.size();
+//     int s1=0;
+//     int s2=0;
+//     int e=n-1;
+//     while(s2<=e){
+//         if(nums[s2]==0){
+//             swap(nums[s1],nums[s2]);
+//             s1++;
+//             s2++;
+//         }
+//         else if(nums[s2]==1 || nums[s2]==2){
+//             s2++;
+//         }
+//         else{
+//             swap(nums[s2],nums[e]);
+//             e--;
+//         }
+//     }
+//     int s3=s1;
+//     while(s3<=e){
+//         if(nums[s3]==1){
+//             swap(nums[s1],nums[s3]);
+//             s1++;
+//             s3++;
+//         }
+//         else{
+//             s3++;
+//         }
+//     }
+//     return nums;
+// }
+
+// int main(){
+//     vector <int> v={0,2,3,2,1,3,0,1,2,3,1,0,3,2,1};
+//     auto x=color_sort(v);
+//     for(int i:x){
+//         cout<<i;
+//     }
+//     return 0;
+// }
+
+
+// first sort 0 and 3 , skip 1 and 2 as we were skipping 1 using 3 pointers
+// then take the left pointer and end pointer after the 0 and 3 are sorted (placed at the end)
+// now use two pointer , go from left pointer to end pointer ,(while(s1<=e)) , and sort one to left as we used to do it for zero using two pointers.
 
 
 
