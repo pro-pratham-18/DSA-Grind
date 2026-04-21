bc451991536909a694da03ddf56d636c8ca1d78f
pro-pratham-18 <itz.prathathamesh18@gmail.com>
2026-04-21 21:33:14 +0530

    approches added

---
+++ Approaches/subArr Max Product/hello.cpp
@@ -0,0 +1,29 @@
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int max_product(vector <int> v){
+//     int n=v.size();
+//     int leftproduct=1;
+//     int rightproduct=1;
+//     int maxProd=v[0];
+//     for(int i=0;i<n;i++){
+//         if(leftproduct==0){
+//             leftproduct=1;
+//         }   
+//         if(rightproduct==0){
+//             rightproduct=1;
+//         }
+//         leftproduct*=v[i];
+//         rightproduct*=v[n-i-1];
+
+//         maxProd=max(max(leftproduct,rightproduct),maxProd);
+//     }
+//     return maxProd ;
+
+// }
+
+
+
+
